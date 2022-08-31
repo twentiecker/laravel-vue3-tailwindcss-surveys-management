57626d41076903015cb085c21713977d76bed483
twentiecker <twentiecker@gmail.com>
2022-08-31 13:11:44 +0700

    first commit

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <div>
+      <img
+        class="mx-auto h-12 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+        alt="Workflow"
+      />
+      <h2
+        class="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        {{ " " }}
+        <router-link
+          :to="{ name: 'Register' }"
+          class="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          register for free
+        </router-link>
+      </p>
+    </div>
+    <form class="mt-8 space-y-6" @submit="login">
+      <div
+        v-if="errorMsg"
+        class="flex items-center justify-between py-3 px-3 bg-red-500 text-white rounded"
+      >
+        {{ errorMsg }}
+        <span
+          @click="errorMsg = ''"
+          class="w-7 h-7 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 19.5l15-15m-15 0l15 15"
+            />
+          </svg>
+        </span>
+      </div>
+
+      <input type="hidden" name="remember" value="true" />
+      <div class="rounded-md shadow-sm -space-y-px">
+        <div>
+          <label for="email-address" class="sr-only">Email address</label>
+          <input
+            id="email-address"
+            name="email"
+            type="email"
+            autocomplete="email"
+            required=""
+            v-model="user.email"
+            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Email address"
+          />
+        </div>
+        <div>
+          <label for="password" class="sr-only">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autocomplete="current-password"
+            required=""
+            v-model="user.password"
+            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center">
+          <input
+            id="remember-me"
+            name="remember-me"
+            type="checkbox"
+            v-model="user.remember"
+            class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+          />
+          <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+            Remember me
+          </label>
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+            <LockClosedIcon
+              class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+              aria-hidden="true"
+            />
+          </span>
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { LockClosedIcon } from "@heroicons/vue/solid";
+import store from "../store";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const user = {
+  email: "",
+  password: "",
+  remember: false,
+};
+
+let errorMsg = ref("");
+
+function login(ev) {
+  ev.preventDefault();
+  store
+    .dispatch("login", user)
+    .then(() => {
+      router.push({
+        name: "Dashboard",
+      });
+    })
+    .catch((err) => {
+      errorMsg.value = err.response.data.error;
+    });
+}
+</script>
---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,8 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <PageComponent title="Surveys">Content goes here</PageComponent>
+</template>
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+</script>
