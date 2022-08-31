1296d63fbdb78a447aa940a84c05023359d29e01
twentiecker <twentiecker@gmail.com>
2022-08-31 21:36:27 +0700

    add v-for surveys

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -26,9 +26,24 @@
         </router-link>
       </div>
     </template>
+    <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+      <div
+        v-for="survey in surveys"
+        :key="survey.id"
+        class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
+      >
+        <img :src="survey.img" alt="" class="w-full h-48 object-cover" />
+        <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
+        <div v-html="survey.description" class="overflow-hidden flex-1"></div>
+      </div>
+    </div>
   </PageComponent>
 </template>
 
 <script setup>
+import store from "../store";
+import { computed } from "vue";
 import PageComponent from "../components/PageComponent.vue";
+
+const surveys = computed(() => store.state.surveys);
 </script>
