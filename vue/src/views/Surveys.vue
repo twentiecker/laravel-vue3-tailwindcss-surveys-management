a87a29781bd9f5c8155be6a90b18e94da986b9d4
twentiecker <twentiecker@gmail.com>
2022-08-31 21:55:56 +0700

    add deleteSurvey function

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -90,4 +90,14 @@ import { computed } from "vue";
 import PageComponent from "../components/PageComponent.vue";
 
 const surveys = computed(() => store.state.surveys);
+
+function deleteSurvey(survey) {
+  if (
+    confirm(
+      `Are you sure you want to delete this survey? Opreation can't be undone!!`
+    )
+  ) {
+    none;
+  }
+}
 </script>
