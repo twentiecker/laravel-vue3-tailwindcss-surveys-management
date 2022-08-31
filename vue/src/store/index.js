96601b41be8f8d180bbdf32f1239677f7d10921b
twentiecker <twentiecker@gmail.com>
2022-08-31 13:55:07 +0700

    add tmpSurveys

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,30 +1,212 @@
 import { createStore } from "vuex";
 import axiosClient from "../axios";
 
+const tmpSurveys = [
+  {
+    id: 100,
+    title: "tes",
+    slug: "tes",
+    status: "draft",
+    image: "",
+    description: "tes",
+    created_at: "2022-08-31 18:00:00",
+    updated_at: "2022-08-31 18:00:00",
+    expire_date: "2022-09-31 18:00:00",
+    questions: [
+      {
+        id: 100,
+        type: "select",
+        question: "tes",
+        description: null,
+        data: {
+          options: [
+            {
+              uuid: "aaa",
+              text: "USA",
+            },
+            {
+              uuid: "aab",
+              text: "Germany",
+            },
+            {
+              uuid: "aac",
+              text: "Georgia",
+            },
+            {
+              uuid: "aad",
+              text: "India",
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        type: "checkbox",
+        question: "tes",
+        description: "tes tes tes tes tes",
+        data: {
+          options: [
+            {
+              uuid: "aba",
+              text: "Javascript",
+            },
+            {
+              uuid: "acb",
+              text: "PHP",
+            },
+            {
+              uuid: "adc",
+              text: "Python",
+            },
+            {
+              uuid: "aed",
+              text: "HTML + CSS",
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        type: "checkbox",
+        question: "tes",
+        description: "tes tes tes tes tes",
+        data: {
+          options: [
+            {
+              uuid: "aba",
+              text: "Javascript",
+            },
+            {
+              uuid: "acb",
+              text: "PHP",
+            },
+            {
+              uuid: "adc",
+              text: "Python",
+            },
+            {
+              uuid: "aed",
+              text: "HTML + CSS",
+            },
+          ],
+        },
+      },
+      {
+        id: 4,
+        type: "radio",
+        question: "tes",
+        description: "tes tes tes tes tes",
+        data: {
+          options: [
+            {
+              uuid: "aba",
+              text: "Javascript",
+            },
+            {
+              uuid: "acb",
+              text: "PHP",
+            },
+            {
+              uuid: "adc",
+              text: "Python",
+            },
+            {
+              uuid: "aed",
+              text: "HTML + CSS",
+            },
+          ],
+        },
+      },
+      {
+        id: 5,
+        type: "checkbox",
+        question: "tes",
+        description: "tes tes tes tes tes",
+        data: {
+          options: [
+            {
+              uuid: "aba",
+              text: "Javascript",
+            },
+            {
+              uuid: "acb",
+              text: "PHP",
+            },
+            {
+              uuid: "adc",
+              text: "Python",
+            },
+            {
+              uuid: "aed",
+              text: "HTML + CSS",
+            },
+          ],
+        },
+      },
+      {
+        id: 6,
+        type: "text",
+        question: "tes",
+        description: null,
+        data: {},
+      },
+      {
+        id: 7,
+        type: "textarea",
+        question: "tes",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+  {
+    id: 200,
+    title: "laravel",
+    slug: "laravel",
+    status: "active",
+    image: "",
+    description: "laravel",
+    created_at: "2022-08-31 18:00:00",
+    updated_at: "2022-08-31 18:00:00",
+    expire_date: "2022-09-31 18:00:00",
+    questions: [],
+  },
+  {
+    id: 300,
+    title: "vue3",
+    slug: "vue3",
+    status: "active",
+    image: "",
+    description: "vue3",
+    created_at: "2022-08-31 18:00:00",
+    updated_at: "2022-08-31 18:00:00",
+    expire_date: "2022-09-31 18:00:00",
+    questions: [],
+  },
+  {
+    id: 400,
+    title: "tailwind",
+    slug: "tailwind",
+    status: "active",
+    image: "",
+    description: "tailwind",
+    created_at: "2022-08-31 18:00:00",
+    updated_at: "2022-08-31 18:00:00",
+    expire_date: "2022-09-31 18:00:00",
+    questions: [],
+  },
+];
+
 const store = createStore({
   state: {
     user: {
       data: {},
       token: sessionStorage.getItem("TOKEN"),
     },
+    surveys: [...tmpSurveys],
   },
   getters: {},
   actions: {
-    // register({ commit }, user) {
-    //   return fetch("http://localhost:8000/api/register", {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Accept: "application/json",
-    //     },
-    //     method: "POST",
-    //     body: JSON.stringify(user),
-    //   })
-    //     .then((res) => res.json())
-    //     .then((res) => {
-    //       commit("setUser", res);
-    //       return res;
-    //     });
-    // },
     register({ commit }, user) {
       return axiosClient.post("/register", user).then(({ data }) => {
         commit("setUser", data);
