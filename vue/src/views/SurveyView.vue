f27d5ce4696e715e18175953f81f9c591402d1b2
twentiecker <twentiecker@gmail.com>
2022-09-03 09:24:53 +0700

    add active chechkbox section

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -103,6 +103,24 @@
                 class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
               />
             </div>
+
+            <!-- status -->
+            <div class="flex item-start">
+              <div class="flex items-center h-5">
+                <input
+                  id="status"
+                  name="status"
+                  type="checkbox"
+                  v-model="model.status"
+                  class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                />
+              </div>
+              <div class="ml-3 text-sm">
+                <label for="status" class="font-medium text-gray-700"
+                  >Active</label
+                >
+              </div>
+            </div>
           </div>
         </div>
       </form>
