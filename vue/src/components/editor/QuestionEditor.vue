db136641d052ff51500c03915f0c2fed10223c45
twentiecker <twentiecker@gmail.com>
2022-09-03 11:23:53 +0700

    add add button

--- vue/src/components/editor/QuestionEditor.vue
+++ vue/src/components/editor/QuestionEditor.vue
@@ -1,5 +1,30 @@
 <template>
-  <div></div>
+  <div class="flex items-center justify-between">
+    <h3 class="text-lg font-bold">{{ index + 1 }}.{{ model.question }}</h3>
+    <div class="flex items-center">
+      <button
+        type="button"
+        @click="addQuestion"
+        class="flex items-center px-3 py-1 mr-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-4 h-4 mr-2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+        Add
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup>
