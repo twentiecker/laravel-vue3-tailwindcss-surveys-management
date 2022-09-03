aedb29983216cc7ffaed3cd7d80702124ccc41c4
twentiecker <twentiecker@gmail.com>
2022-09-03 11:16:14 +0700

    define setup script

---
+++ vue/src/components/editor/QuestionEditor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  question: Object,
+  index: Number,
+});
+
+const emit = defineEmits(["change", "addQuestion", "deleteQuestion"]);
+
+const model = ref(JSON.parse(JSON.stringify(props.question)));
+</script>
+
+<style lang="scss" scoped></style>
