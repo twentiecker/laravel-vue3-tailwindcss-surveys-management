b51835eab8e13bb4b9d60c0d5fdd539a00d90fec
twentiecker <twentiecker@gmail.com>
2022-09-05 08:13:28 +0700

    add saveSurvey function

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -195,11 +195,12 @@
 import { v4 as uuidv4 } from "uuid";
 import store from "../store";
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
 
+const router = useRouter();
 const route = useRoute();
 
 let model = ref({
@@ -239,6 +240,15 @@ function questionChange(question) {
       return JSON.parse(JSON.stringify(question));
     }
     return q;
+  });
+}
+
+function saveSurvey() {
+  store.dispatch("saveSurvey", model.value).then(({ data }) => {
+    router.push({
+      name: "SurveyView",
+      params: { id: data.data.id },
+    });
   });
 }
 </script>
