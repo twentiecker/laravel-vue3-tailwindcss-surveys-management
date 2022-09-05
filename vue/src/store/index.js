3f0f27e6b8e544f28ba424dcb1ed4de4e2e497ae
twentiecker <twentiecker@gmail.com>
2022-09-05 10:19:33 +0700

    add saveSurvey, updateSurvey fun

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -208,6 +208,23 @@ const store = createStore({
   },
   getters: {},
   actions: {
+    saveSurvey({ commit }, survey) {
+      let response;
+      if (survey.id) {
+        response = axiosClient
+          .put(`/survey/${survey.id}`, survey)
+          .then((res) => {
+            commit("updateSurvey", res.data);
+            return res;
+          });
+      } else {
+        response = axiosClient.post("/survey", survey).then((res) => {
+          commit("saveSurvey", res.data);
+          return res;
+        });
+      }
+      return response;
+    },
     register({ commit }, user) {
       return axiosClient.post("/register", user).then(({ data }) => {
         commit("setUser", data);
@@ -228,6 +245,17 @@ const store = createStore({
     },
   },
   mutations: {
+    saveSurvey: (state, survey) => {
+      state.surveys = [...state.surveys, survey.data];
+    },
+    updateSurvey: (state, survey) => {
+      state.surveys = state.surveys.map((s) => {
+        if (s.id == survey.data.id) {
+          return survey.data;
+        }
+        return s;
+      });
+    },
     logout: (state) => {
       state.user.data = {};
       state.user.token = null;
