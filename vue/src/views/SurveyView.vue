589631507b8053ca108fca9f24cec7dcb83a5bcc
twentiecker <twentiecker@gmail.com>
2022-09-05 10:46:05 +0700

    add onImageChoose fun

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -21,9 +21,9 @@
               </label>
               <div class="mt-1 flex items-center">
                 <img
-                  v-if="model.image"
-                  :src="model.image"
-                  :alt="model.image"
+                  v-if="model.image_url"
+                  :src="model.image_url"
+                  :alt="model.title"
                   class="w-64 h-48 object-cover"
                 />
                 <span
@@ -51,6 +51,7 @@
                 >
                   <input
                     type="file"
+                    @change="onImageChoose"
                     class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer"
                   />
                   Change
@@ -216,6 +217,20 @@ if (route.params.id) {
   model.value = store.state.surveys.find(
     (s) => s.id === parseInt(route.params.id)
   );
+}
+
+function onImageChoose(ev) {
+  const file = ev.target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    // the field to send on backend and apply validations
+    model.value.image = reader.result;
+
+    // the field to display here
+    model.value.image_url = reader.result;
+  };
+  reader.readAsDataURL(file);
 }
 
 function addQuestion(index) {
